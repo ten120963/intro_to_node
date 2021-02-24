2b89b8f4d87e94ad52af7f79dbc29e41486e4712
Thomas Nowak <ten120963@gmail.com>
2021-02-24 11:47:57 -0500

    updated package.json, procfile, and hello.js PORT

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
 
 http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);	
@@ -21,6 +22,6 @@ http.createServer(function (req, res) {
 		console.log("...Incoming Request: " + req.url);
 		return res.end();
 	});
-}).listen(8080);
+}).listen(PORT);
 
 console.log("Server Listening on Port 8080 ...");
